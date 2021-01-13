dbb5d46252a1d636249e5d9ca31ddadf787dc756
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2021-01-13 13:31:23 +0300

    added functions and operators warm-ups

---
+++ introduction/functions.hs
@@ -0,0 +1,5 @@
+main = putStrLn "Hello, world!"
+
+lenVec3 x y z = sqrt (x^2 + y^2 + z^2)
+
+sign x = if x > 0 then 1 else if x < 0 then (-1) else 0
---
+++ introduction/operators.hs
@@ -0,0 +1,5 @@
+infixl 6 *+*
+a *+* b = a^2 + b^2
+
+infixl 6 |-|
+x |-| y = abs (x - y)
